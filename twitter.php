e122dbab6dd2a7598f718f33f5b42ee1e319a02c
Bilal Quadri <bilalquadri92@gmail.com>
2012-03-31 10:57:55 -0400

    Twitter set up

--- twitter.php
+++ twitter.php
@@ -1,10 +1,28 @@
 <?php
   require_once('config.php');
+  require_once('mongo.php');
+
+  $collection = $db->selectCollection('twitter');  
 
   function getTweetSet($lat, $lng, $rad) {
-  $tweetSet = json_decode(file_get_contents("http://search.twitter.com/search.json?q="."a"."&geocode=".$lat.",".$lng.",".$rad."mi&result_type=mixed&count=50&lang=en"));	
+    $tweetSet = json_decode(file_get_contents("http://search.twitter.com/search.json?q="."a"."&geocode=".$lat.",".$lng.",".$rad."mi&result_type=mixed&count=10&lang=en"));	
 
-  $tweetSet->
+    $results = $tweetSet->results;
+
+    foreach ($results as $tweet) {
+      $handle = $tweet->from_user;
+      $full_name = $tweet->from_user_name;
+      $text = $tweet->text;
+
+      $entry = array(
+        "handle" => $handle,
+        "name" => $full_name,
+        "tweet" => $text,
+        "lat" => $lat,
+        "lng" => $lng
+      );
+
+      $collection->insert($entry);
+    }
   }
 
-?>
