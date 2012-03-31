e45cb985cac37a642abb14a437eecf004faaef3e
Bilal Quadri <bilalq@ubuntu.(none)>
2012-03-31 01:51:10 -0400

    Added content

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+global $etsy_appid;
+$etsy_appid='ge3dbdf6zfgo29ugsom24iq2';
+$twitter_key='cXX3HXOAvpmIExQa9SIUoQ';
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>DataVu</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8">
+  </head>
+  <body>
+    <div id="wrapper">
+      <h1>DataVu <small>Twitter</small></h1>
+    </div>
+  </body>
+</html>
