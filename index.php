f091f2c369fe7040c3c52ecb7a0e385155ca7be2
Bilal Quadri <bilalquadri92@gmail.com>
2012-03-31 05:29:51 -0400

    Take it

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>DataVu</title>
-    <link rel="stylesheet" href="css/style.css" type="text/css" charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   </head>
   <body>
     <div id="wrapper">
-      <h1>DataVu <small>Twitter</small></h1>
+      <div class="page-header"><h1>DataVu <small>Twitter</small></h1></div>
     </div>
   </body>
 </html>
