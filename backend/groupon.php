94449bcf2ca6b7ce1e80b65912d3097c1e8a73cb
Jerry Reptak <jreptak@gmail.com>
2012-03-31 12:45:20 -0400

    Groupon Mongo

--- backend/groupon.php
+++ backend/groupon.php
@@ -2,6 +2,14 @@
 
 require_once 'debug.php';
 
+require_once 'mongo.php';
+
+$collection = $db->selectCollection('groupon');
+
+$collection->insert(grouponGetDealsByDivision('central-jersey');
+$collection->insert(grouponGetDealsByDivision('north-jersey');
+
+
 #Hard Coded Bad Stuff
 function isVerifiedState($location) {
 	$hardcodeSadness = "New Jersey";
@@ -12,11 +20,48 @@ function isVerifiedState($location) {
 }
 #End of the Bad Hard Coding
 
+
+function grouponGetDealsByDivision($division_id) {
+	$base = 'deals.json';
+	$request = $base . '?division_id=' . $division_id;
+
+	$jsons = grouponRequest($request);
+
+	//Something failed
+	if(empty($jsons))  {
+		echo "Failed JSON Request Son!";
+		return '';
+	}
+
+	$deals = json_decode($jsons);
+
+	return encodeJson($deals);
+}
+
+function grouponRequest($request) {
+	require_once 'config.php';
+
+	$apikey = '&client_id=' . $groupon_appid;
+	$base = 'https://api.groupon.com/v2/';
+	$url = $base . $request . $apikey;
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response_body = curl_exec($ch);
+	$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$ret_status = intval($status);
+
+	if($ret_status != 200) {
+		return '';
+	}
+
+	return $response_body;
+}
+
 function encodeJsonDeal($deal) {
 
 	if(!$deal->isSoldOut) {
 		foreach($deal->options as $option) {
-
 
 			//Make sure it isn't sold out
 			if(!$option->isSoldOut) {
@@ -35,7 +80,7 @@ function encodeJsonDeal($deal) {
 							"dealUrl" => $deal->dealUrl,
 							"img" => $deal->sidebarImageUrl,
 							"description" => $option->details[0]->description,
-#							"pitchHtml" => $deal->pitchHtml,
+							#							"pitchHtml" => $deal->pitchHtml,
 							"lng" => $location->lng,
 							"lat" => $location->lat,
 						);
@@ -61,43 +106,5 @@ function encodeJson($deals) {
 
 	return json_encode($arrDeals);
 }
-
-function grouponGetDealsByDivision($division_id) {
-	$base = 'deals.json';
-	$request = $base . '?division_id=' . $division_id;
-
-	$jsons = grouponRequest($request);
-
-	//Something failed
-	if(empty($jsons))  {
-		echo "Failed JSON Request Son!";
-		return '';
-	}
-
-	$deals = json_decode($jsons);
-
-	return encodeJson($deals);
-}
-
-function grouponRequest($request) {
-	include 'config.php';
-
-	$apikey = '&client_id=' . $groupon_appid;
-	$base = 'https://api.groupon.com/v2/';
-	$url = $base . $request . $apikey;
-
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$response_body = curl_exec($ch);
-	$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	$ret_status = intval($status);
-
-	if($ret_status != 200) {
-		return '';
-	}
-
-	return $response_body;
-}
-
 
 ?>
