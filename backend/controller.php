ab4417e20d46378e197c9a878293747d0ef319ce
Bilal Quadri <bilalquadri92@gmail.com>
2012-03-31 14:57:55 -0400

    balh

--- backend/controller.php
+++ backend/controller.php
@@ -1,32 +1,25 @@
 <?php
 
-if(!empty($_POST['lens'])) {
+require_once 'mongo.php';
 
-	require_once 'mongo.php';
-	
-	$socialtype = $_POST['lens'];
+$lens = $_POST['lens'];
 
-	switch($lens) {
-		case "twitter":
-			$collection = $db->selectCollection('twitter');
-			echo $collection->find();
+switch($lens) {
+  case "twitter":
+    $collection = $db->selectCollection('twitter');
+    echo $collection->find();
 
-			break;
-		case "groupon":
-			$collection = $db->selectCollection('groupon');
-			echo $collection->find();
+    break;
+  case "groupon":
+    $collection = $db->selectCollection('groupon');
+    echo $collection->find();
 
-			break;
-		case "craigslist":
-			$collection = $db->selectCollection('craigslist');
+    break;
+  case "craigslist":
+    $collection = $db->selectCollection('craigslist');
 
-			break;
-		default:
-			echo "Wrong Lens type";
-	}
-
-
+    break;
+  default:
+    echo "Wrong Lens type";
 }
 
-
-?>
