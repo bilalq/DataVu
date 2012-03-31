886f41cf22b5724d74e4c87e42faff91e5f22758
Bilal Quadri <bilalquadri92@gmail.com>
2012-03-31 05:49:26 -0400

    Variable name changes

--- googleCoord.php
+++ googleCoord.php
@@ -3,8 +3,8 @@ function googleLocation($address) {
   $geocode=file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.urlEncode($address).'&sensor=false');
   $output= json_decode($geocode);
   $lat = $output->results[0]->geometry->location->lat;
-  $long = $output->results[0]->geometry->location->lng;
-  $latlong = array('lat' => $lat, 'long' => $long);
+  $lng = $output->results[0]->geometry->location->lng;
+  $latlng = array('lat' => $lat, 'lng' => $lng);
   return $latlong;
 }
 
