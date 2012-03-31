55c6f83e86722eaba5029ca0382c6468d5fc49b0
Bilal Quadri <bilalquadri92@gmail.com>
2012-03-31 05:31:29 -0400

    added css and js

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>DataVu</title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" href="js/main.js"></script>
   </head>
   <body>
     <div id="wrapper">
