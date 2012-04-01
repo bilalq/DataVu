ecc84dfaaccdd60c12da363de56d3d3a42178dd5
Bilal Quadri <bilalquadri92@gmail.com>
2012-04-01 12:35:54 -0400

    Bing maps stuff

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0"></script>
     <script type="text/javascript" src="js/main.js"></script>
+
   </head>
   <body>
     <div id="wrapper">
       <div class="page-header"><h1>DataVu <small>Twitter</small></h1></div>
       <div id="preload"></div>
 
-      <canvas id="map"> </canvas>
+      <div id="myMap"></div>
       <div id="sidebar" class="pull-right well">
         <ul id="info" class="nav nav-list">
           <li class="nav-header">Lens</li>
--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
 $(document).ready( function(){
-  
+
   //Cached selectors
   var infoList = $('div#info');
   var twitterData = "";
   var grouponData = "";
-  var preload = $('div#preload');
-  var canvas = document.getElementById('map');
-  var ctx = canvas.getContext('2d'); 
-  canvas.width = 400;
-  canvas.height = 600;
-  ctx.width = 400;
-  ctx.height = 600;
   var images = new Array();
   var groupImages = new Array();
+  var theWindow = $(window);
+  var map = null;
 
-  //Navbar links
-  var twitLink = $('li#twitter');
-  var groupLink = $('li#groupon');
+  function getMap() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+      credentials: 'Assd2akCabc9zZOrMER5t5DUkIHfUhvLuUawIyLYrno_IlOa4RU7iVYtVpgYbGn1',
+      enableClickableLogo: false,
+      enableSearchLogo: false,
+      disablePanning: true,
+      disableZooming: true,
+      showDashboard: false,
+      showScalebar: false,
+      width: '800px',
+      height: '400px',
+      mapTypeId: Microsoft.Maps.MapTypeId.mercator,
+      zoom: 5,
+      center: new Microsoft.Maps.Location(40.0393264770508, -74.5893569946289)
+    });
+  };  
 
   $.ajax({
     type: 'POST',
@@ -50,90 +58,6 @@ $(document).ready( function(){
           });
         }
       }
-  });
-  /*
-   * Twitter handling
-   * Draws tweets to the map
-   */
-  twitLink.on('click', function() {
-    twitLink.addClass('active');
-    groupLink.removeClass('active');
-
-    if (twitterData.length == 0) {
-      $.ajax({
-        type: 'POST',
-        url: 'backend/controller.php',
-        data: {'lens': 'twitter'},
-        success: function(response) {
-            twitterData = $.parseJSON(response);
-        }
-      });
-    }
-    for (var i = 0; i < twitterData.length; i++) {
-      var current = twitterData[i];
-      var lat = current.lat;
-      var lng = current.lng;
-      var pic = current.img;
-      var tweetID = current._id.$id;
-      
-      var xOff = 300.0 * 0.9 * (-lng - 74) / (0.95) + 10 - random(20);
-      var yOff = 600.0 * 0.9 * (41.39 - lat)/(41.39-39.0) + 10 - random(20);
-
-      ctx.drawImage(images[i], 0, 0, 48, 48, xOff, yOff, 48, 48);
-
-      //$('<img id="'+tweetID+'" src="'+pic+'"/>')
-        //.append(canvas)
-        //.css({
-          //'position': 'absolute',
-          //'top': 550.0 * (41.39 - lat)/(41.39-39.0),
-          //'left': 800 * (-lng - 74) / (0.95)
-        //});
-
-    };
-  });
-
-
-  /*
-   * Groupon handler
-   * Finds deals and draws related images on map.
-   */
-  groupLink.on('click', function() {
-    groupLink.addClass('active');
-    twitLink.removeClass('active');
-    $.ajax({
-      type: 'POST',
-      url: 'backend/controller.php',
-      data: {'lens': 'groupon'},
-      success: function(response) {
-          grouponData = $.parseJSON(response);
-          for (var i = 0; i < grouponData.length; i++) {
-
-            var current = grouponData[i];
-            var glat = current.lat;
-            var glng = current.lng;
-            var xgOff = 300.0 * 0.9 * (-glng - 74) / (0.95) + 10 - random(20);
-            var ygOff = 600.0 * 0.9 * (41.39 - glat)/(41.39-39.0) + 10 - random(20);
-            ctx.drawImage(groupImages[i], 10, 10, 48, 48, xgOff, ygOff, 48, 48);
-            //var current = grouponData[i];
-            //var lat = current.lat;
-            //var lng = current.lng;
-            //var expires = current.expires;
-            //var dealurl = current.dealurl;
-            //var pic = current.img;
-            //var title = current.title;
-            //var desc = current.description;
-            //var groupID = current._id.$id;
-          }
-      }
-    });
-  });
-
-
-  /*
-   * Generates random number from 1 to X.
-   */
-  function random (x) {
-    return Math.floor(Math.random()*x);
-  }
+  }); //Preloads images
 
 }); //end docReady
